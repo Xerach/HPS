fde3f6cad86cc222ab5b3366feb5f85cc66cb13a
Xerach <Xerach@10.230.175.226>
2013-02-08 21:45:03 +0000

    FIRST GOOD

---
+++ src/operator/Numbers.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operator;
+
+import interfaces.Node;
+
+/**
+ *
+ * @author Xerach
+ */
+public class Numbers extends Node{
+    private double num;
+    
+    void numbers(double number){
+        num = number;
+    }
+    
+    @Override
+    public double evaluate() {
+        return getnumber();
+    }
+    
+    public double getnumber(){
+        return num;
+    }
+    
+    public void setnumber(double number){
+        num = number;
+    }
+}
